6c46ad2fbe29e17075b909836ddcc1a309001b10
Nikki <n.hritz77@gmail.com>
2024-06-24 11:06:01 -0500

    pt one converts num to string and displays the length in case of a decimal or not/ pt two takes string and applys chaing methods using temp literals  to manipulate the strings/ part three combines multiple chaining methods and cuts initals with string and alt methods

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,19 @@
 let num = 1001;
 
 //Returns 'undefined'.
-console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+let numAsString = String(1001);
+console.log("The number 1001 becomes " + numAsString + " now as data type: \n\t" + typeof numAsString + "\n");
+console.log("\t"+ numAsString.length);
+//console.log(num, "has", num.length, "didgets.");
+console.log("\n", numAsString, "has", numAsString.length, "didgets.");
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).includes('.')) {
+    console.log("\n\t",String(num).length - 1);
+} else {
+    console.log("\n\t", String(num).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,20 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log( language.slice(0,1) + language.slice(4,5) );
 //2. Without using slice(), use method chaining to accomplish the same thing.
 
+let initials = language.replace("Java", "J").replace("Script","S");
+console.log(initials);
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbrviation for '${language}' is '${initials}'.`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
-
+let funString = "  HEY!  if you are happy and you know it\n\t       clap your hands       ";
+let moreFunString = funString.toUpperCase().trim().slice(4,71);
+console.log(funString.length);
+console.log(moreFunString);
 //Part Three section Two
 
-//1. Use the string methods you know to print 'Title Case' from the string 'title case'.
-
+//1. Use the string methods you know to print 'Title Case' from the string 'title case'
 let notTitleCase = 'title case';
+console.log(notTitleCase.toUpperCase());
