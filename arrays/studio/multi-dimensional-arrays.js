32e097cdcc29fd6492d2676740daf662834d2ac9
Nikki <n.hritz77@gmail.com>
2024-07-03 16:11:48 -0500

    Each portion of the studio now runs as expected

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,3 +1,6 @@
+const input = require('readline-sync');
+
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
@@ -5,10 +8,42 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+let foodArr = food.split(",").sort();
+let equipmentArr = equipment.split(",").sort();
+let petsArr = pets.split(",").sort();
+let sleepAidsArr = sleepAids.split(",").sort();
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+
+let cargoHold = [];
+cargoHold.push(foodArr, equipmentArr,  petsArr, sleepAidsArr);
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
 
+let userNum = input.question(`Please choose a cabinet number (0-3):
+    0 - Food
+    1 - Equipment
+    2 - Pets
+    3 - Sleep Aids
+    `);
+
+    let selectedCabinet;
+
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+
+if (userNum < 0 || userNum > 3){
+    console.log("Invalid choice! Please pick a cabinet 0 - 3: ")
+}else{
+    selectedCabinet = cargoHold[userNum];
+    console.log(selectedCabinet);
+let selectedItem = input.question("Which item from your selected cabned do you want?");//5)starts here
+
+let containsText = "DOES ";
+if (!selectedCabinet.includes(selectedItem)){
+    containsText += "NOT";
+}
+console.log(`The cabinet ${containsText} contain your selected item: ${selectedItem}`);
+}
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,28 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let modString = str.slice(3) + str.slice(0, 3);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The string '${str}' has now been converted to '${modString}'. `);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let userNum = input.question("How many characters should be moved? ");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+let numTooLong = false;
+
+if (userNum > str.length) {
+    numTooLong = true;
+    str.num = 3;
+}
+
+let usermodString = str.slice(userNum) + str.slice(0, userNum);
+
+let result = `'${str}' string was converted to '${usermodString}'.`;
+
+if(numTooLong){
+    result += " (The number you entered is bigger than the provided strings length, so we just moved the first 3 letters.)";
+}
+
+console.log(result);
