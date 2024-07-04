b2422309ee431e61ee0b322f35403df5b370849e
Nikki <n.hritz77@gmail.com>
2024-07-03 22:50:22 -0500

    Loops exercise runs and is complete

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,21 +4,49 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
+}
 
+for (let i = 12; i >= -14; i -= 2) {
+  console.log(i);
+}
 
-
+for (let i = 50; i >= 20; i--) {
+  if (i % 3 === 0)
+    console.log(i);
+}
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
-  b. Print each character of the string - in reverse order - to a new line. */ 
+  b. Print each character of the string - in reverse order - to a new line. */
 
+let str1 = 'LaunchCode';
+let arr1 = [1, 5, 'LC101', 'blue', 42];
 
+for (i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+}
 
-
-
+for (i = 0; i < str1.length; i++) {
+  console.log(str1[i]);
+}
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+let otherArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [], odds = [];
+
+for (i = 0; i < otherArr.length; i++) {
+  if (i % 2 === 0) {
+    evens.push(otherArr[i]);
+  } else {
+    odds.push(otherArr[i]);
+  }
+}
+  console.log(evens);
+  console.log(odds);
