45c5f1ffdc253572b23d9583c87f77936924322d
Nikki <n.hritz77@gmail.com>
2024-07-21 19:00:16 -0500

    test-student-labs now completegradeLabs(studentLabs2);gradeLabs(studentLabs2);

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,6 +4,11 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+  try{
+    gradeLabs(studentLabs2);
+  }catch(error){
+    console.log("Error thrown");
+  }
 }
 
 let studentLabs = [
@@ -20,5 +25,27 @@ let studentLabs = [
     }
   }
 ];
+let studentLabs2 = [
+  {
+    student: 'Blake',
+    myCode: function (num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Jessica',
+    runLab: function (num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Mya',
+    runLab: function (num) {
+      return num * num;
+    }
+  }
+];
 
 gradeLabs(studentLabs);
+// gradeLabs(studentLabs2);
+
