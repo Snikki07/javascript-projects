7a851220fa28591ada2432eb40e66128f840fa3f
Nikki <n.hritz77@gmail.com>
2024-07-22 16:07:18 -0500

    Ex 02 completed

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,15 +3,15 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, age = 85, mass = 45){
      this.name = name;
      this.age = age;
      this.mass = mass;
    }
 }
 
-let tortoise = new Astronaut('Speedy', 120);
+let tortoise = new Astronaut('Speedy', );
 
 console.log(tortoise.name, tortoise.age, tortoise.mass);
 
-// What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+// What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!, unless it has defult value or assigned properties its dark
